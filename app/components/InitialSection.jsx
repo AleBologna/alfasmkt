e6cfd176c7ef748915da3e4fb61e2ee5654e87ed
Alejandro G. Bologna <alejandrobologna2003@gmail.com>
2024-09-22 11:20:00 -0300

    prueba

--- app/components/InitialSection.jsx
+++ app/components/InitialSection.jsx
@@ -22,7 +22,7 @@ export default function InitialSection() {
             </div>
 
             <div className="w-full my-5 flex justify-center sm:flex-row sm:items-end sm:my-11">
-              <a href="#ContactUsSection">
+              <a href="#ContactUsSectionx">
                 <button
                   type="button"
                   class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center me-2 mb-2"
